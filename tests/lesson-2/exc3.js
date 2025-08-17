4fac7eb8044d7604cabd48cf8fa78edd1ea09b10
Tuyet <tuyetlt306@gmail.com>
2025-08-17 22:52:52 +0700

    {feat:add file to lesson-2}

---
+++ tests/lesson-2/exc3.js
@@ -0,0 +1,6 @@
+const number = 39
+if (number > 0) {console.log("Giá trị bạn nhập là số dương")}
+if (number < 0) {
+console.log("Giá trị bạn nhập là số âm")}
+if (number == 0) {
+console.log("Giá trị bạn nhập là số 0")}
---
+++ tests/lesson-2/exc4.js
@@ -0,0 +1,6 @@
+const tall = 149 //unit: cm
+let number = tall%100
+let standard_weight = number * 9 / 10
+let max_weight = number
+let min_weight = number * 8 / 10
+console.log(standard_weight, max_weight, min_weight)
