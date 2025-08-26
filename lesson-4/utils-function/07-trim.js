136a821f87e0838b1beb0ae3128edbdde940cc1c
admin <admin@admins-MacBook-Pro-2.local>
2025-08-26 11:18:48 +0700

    core: add file to lesson4

---
+++ lesson-4/utils-function/07-trim.js
@@ -0,0 +1,14 @@
+const name = ' Nguyễn Văn A ';
+const userInput = ' 12345 ';
+const email = ' example@gmail.com ';
+
+//1
+const trimName = name.trim();
+console.log("'" + trimName + "'");
+//2
+const trimUser = userInput.trim();
+console.log("'" + trimUser + "'");
+//3
+const trimEmail = email.trim();
+console.log("'" + trimEmail + "'");
+
